457fe9a8ad9672dd746223376a452cf20a617c96
Colin Jack <colin.jack@gmail.com>
2012-10-11 22:01:01 +0100

    Added some comments to explain what namespace.js is about.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// TODO: Logging wrapper
 var winston = require('winston');
 var libNamespace = require('./lib/namespace');
 
 var configureResourcesInDirectory = function(directoryPath, done) {
     try {
-        // make the namespace for the lib directory global for easier access
         global.lib = libNamespace;
-        var registerAllResourcesInDirectory = libNamespace.require('registerAllResourcesInDirectory');
 
+        var registerAllResourcesInDirectory = libNamespace.require('registerAllResourcesInDirectory');
         registerAllResourcesInDirectory(directoryPath, app, done);
     } catch (ex) {
         winston.error("Failed to configure resources due to error: " + ex);
