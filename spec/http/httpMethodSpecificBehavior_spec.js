97474e95fb4632b61995288e0e457a4377fada1a
Colin Jack <colin.jack@gmail.com>
2012-10-11 21:40:42 +0100

    Breaking out http method specific behavior to a seperate set of objects and adding tests.

--- spec/http/httpMethodSpecificBehavior_spec.js
+++ spec/http/httpMethodSpecificBehavior_spec.js
@@ -8,41 +8,49 @@ var getHttpMethodSpecificBehavior = function(httpMethod) {
 
 var shouldNotArgumentHandlerArguments = function() {
     return function(err, underTest) {
-        var methodArguments = []
-        underTest.augmentHandlerArguments(methodArguments);
+        var methodArguments = [];
+        var fakeRequest = { body: 5};
+        
+        underTest.augmentHandlerArguments(methodArguments, fakeRequest);
+        
         assert.equal(methodArguments.length, 0);
     }
 };
 
-var shouldSendReturnedObjectAsResponseBody = function() {
-  return function(err, underTest) {
-        var returned = {};
-        var sent;
-
-        var responseSpy = {
-            send : function(toSend) {
-                sent = toSend;
-            }
-        }
-
-        underTest.prepareResponse(returned, responseSpy);
-
-        assert.equal(sent, returned);
-    }  
-}
-
-var getHttpMethodForHandler = function(handlerMethodName) {
-    return getHttpMethodToUseForHandler({}, handlerMethodName);
-}
+var shouldArgumentHandlerArguments = function() {
+    return function(err, underTest) {
+        var methodArguments = [];
+        var fakeRequest = { body: 5};
+        
+        underTest.augmentHandlerArguments(methodArguments, fakeRequest);
+        
+        assert.equal(methodArguments.length, 1);
+        assert.equal(methodArguments.pop(), fakeRequest.body);
+    }
+};
 
 vows.describe('http method specific behavior').addBatch({
     'when you use behavior for a get request': {
         topic: getHttpMethodSpecificBehavior("get"),
 
         'should not augment the handler arguments' : shouldNotArgumentHandlerArguments(),
-
-        'should send returned object as response body' : shouldSendReturnedObjectAsResponseBody()
     },
 
+    'when you use behavior for a delete request': {
+        topic: getHttpMethodSpecificBehavior("delete"),
 
+        'should not augment the handler arguments' : shouldNotArgumentHandlerArguments(),
+    },
+
+    'when you use behavior for a put request': {
+        topic: getHttpMethodSpecificBehavior("put"),
+
+        'should not augment the handler arguments' : shouldArgumentHandlerArguments(),
+    },
+
+    'when you use behavior for a post request': {
+        topic: getHttpMethodSpecificBehavior("post"),
+
+        'should not augment the handler arguments' : shouldArgumentHandlerArguments(),
+    }
 }).export(module);
