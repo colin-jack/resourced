1fb390621ed6a91d1a8a594c9068ec6bdd7d8abf
Colin Jack <colin.jack@gmail.com>
2012-11-04 14:39:13 +0000

    updated grunt file.

--- grunt.js
+++ grunt.js
@@ -1,30 +1,39 @@
-module.exports = function (grunt) {
-    "use strict";
+module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        lint: {
-            all: ["grunt.js", "lib/**/*.js"]
-        },
-        jshint: {
-            options: {
-                browser: true,
-                curly: true,
-                eqeqeq: true,
-                newcap: true,
-                undef: true,
-                eqnull: true,
-                node: true,
-                strict: false,
-                asi: true
-            },
-            globals: {
-                exports: true,
-                lib: true
-            }
-        }
-    });
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'default'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        // immed: true,
+        // latedef: true,
+        // newcap: true,
+        noarg: true,
+        // sub: true,
+        //undef: true,
+        // boss: true,
+        // eqnull: true,
+        node: true,
+        asi: true,
+        strict: false
+      },
+      globals: {
+        exports: true,
+        lib: true
+      }
+    }
+  });
 
-    // Default task.
-    grunt.registerTask('default', 'lint');
+  // Default task.
+  grunt.registerTask('default', 'lint');
+
 };
