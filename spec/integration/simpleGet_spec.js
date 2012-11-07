8a4011e04e9bca05209206ddd4087fff9d837228
Colin Jack <colin.jack@gmail.com>
2012-11-07 18:51:32 +0000

    Preparing for next phase of work on integration tests.

--- spec/integration/simpleGet_spec.js
+++ spec/integration/simpleGet_spec.js
@@ -2,19 +2,19 @@ var request = require('testresources'),
     express = require('express'),
     testUtil = require('./testUtil');
 
-describe('When you send get request to simple resource', function(){
+describe.skip('When you send get request to simple resource', function(){
     beforeEach(require('./registerTestResources'))
 
-    it('should respond with expected json when URI is correct', function(done) {
+    it.skip('should respond with expected json when URI is correct', function(done) {
         var body = { name: "spot", id: 5 };
 
         request(app).get('/puppies/5')
             .expectBody(body)
             .expectCached('public', 5)
-            .end(testUtil.assertNoError(done));
+            .run(testUtil.assertNoError(done));
     })
 
-    it('should raise appropriate error if argument does not match expectations', function(done) {
+    it.skip('should raise appropriate error if argument does not match expectations', function(done) {
         var expectedBody = {
             property: "id",
             message: "The value must be numeric."
@@ -23,6 +23,6 @@ describe('When you send get request to simple resource', function(){
         request(app).get('/puppies/bob')
             .expectStatus(400)
             .expectBody(expectedBody)
-            .end(done); // testUtil.assertError(done)
+            .run(done); // testUtil.assertError(done)
     })
 })
--- spec/unit/createRequestHandler/validateParams/invalid_body.js
+++ spec/unit/createRequestHandler/validateParams/invalid_body.js
@@ -5,49 +5,45 @@ var fixture = require('./../../testFixture'),
     validationTestUtil = require('./validationTestUtil'),
     underTest = fixture.require('validateBody');
 
-describe('validateBody', function() {
-    describe("When the request body is validated", function() {
+describe('validateBody - ', function() {
+    describe("when the request body is validated", function() {
         var responseSpy, handlerDefinition, returned;
 
         beforeEach(function() {  
             responseSpy = responseTestUtil.createResponseSpy();
-        });
+        });  
 
-  
+        describe("and it is invalid", function() {
+            var returned;
 
-        // describe("and it is invalid", function() {
-        //     var returned;
+            beforeEach(function() {  
+                var fakeRequest = testUtil.createFakeRequest();           
+                fakeRequest.body = createInvalidRequestBody();
 
-        //     beforeEach(function() {  
-        //         var fakeRequest = testUtil.createFakeRequest();           
-        //         fakeRequest.body = createInvalidRequestBody();
+                var handlerDefinition = handlerDefinitionObjectMother.createWithIdBodyRules();
 
-        //         fakeRequest.params = {id: 5};
+                returned = underTest(fakeRequest, responseSpy, handlerDefinition);
+            });
 
-        //         var handlerDefinition = handlerDefinitionObjectMother.createWithIdBodyRules();
-
-        //         returned = underTest(fakeRequest, responseSpy, handlerDefinition);
-        //     });
-
-        //     it('should fail because of invalid query string', function() {
-                    // var expectedBody = { 
-                    //     message: "The request body was not valid.",
-                    //     details: {
-                    //         status: { 
-                    //             message: 'The value must be a string.',
-                    //             type: 'not_a_string',
-                    //             value: 5 },
-                    //         age: { 
-                    //             message: 'The value must be populated.',
-                    //             type: 'not_populated',
-                    //             value: null 
-                    //         } 
-                    //     }
-                    // }
+            it('should fail because of invalid query string', function() {
+                var expectedBody = { 
+                    message: "The request body was not valid.",
+                    details: {
+                        status: { 
+                            message: 'The value must be a string.',
+                            type: 'not_a_string',
+                            value: 5 },
+                        age: { 
+                            message: 'The value must be populated.',
+                            type: 'not_populated',
+                            value: null 
+                        } 
+                    }
+                }
                 
-        //         validationTestUtil.shouldCorrectlyFailValidation(responseSpy, expectedBody, returned);
-        //     });
-        // });
+                validationTestUtil.shouldCorrectlyFailValidation(responseSpy, expectedBody, returned);
+            });
+        });
 
         function createInvalidRequestBody() {
             return {
@@ -56,6 +52,4 @@ describe('validateBody', function() {
             };
         };
     });
-
-    // TODO: Body included and previous argument undefined
 });
