d253ee4c85a60d99e25bed0b4488d7e3f8450c69
Colin Jack <colin.jack@gmail.com>
2012-11-08 19:08:57 +0000

    Another PUT validation spec.

--- spec/integration/resources/puppies/validation/puppySchema.js
+++ spec/integration/resources/puppies/validation/puppySchema.js
@@ -2,7 +2,7 @@ var mustBe = require('rules').mustBe;
 var now = require('rules').now;
 
 module.exports = {
-    name: mustBe().populated().string({ maxLength: 50 }),
+    name: mustBe().populated().string({ minLength: 50 }),
     
     cell: mustBe().string()
 };
--- spec/integration/simplePut_spec.js
+++ spec/integration/simplePut_spec.js
@@ -12,12 +12,35 @@ describe('when you send put request to simple resource which validates the body'
             .run(done);
     })
 
-    it('should raise appropriate error if body is missing', function(done) {
+    it('should raise appropriate error if body is invalid', function(done) {
         var requestBody = {
             name: "too short",
             cell: true
         }
 
+        var expectedResponseBody = { 
+            message: 'The request body was not valid.',
+            details: { 
+                name: { 
+                    message: 'The length cannot be less than 50.',
+                    type: 'outside_length_constraint',
+                    value: 'too short' 
+                },
+                cell: { 
+                    message: 'The value must be a string.',
+                    type: 'not_a_string',
+                    value: true 
+                } 
+            }   
+        };
+
+        request(app).put('/puppies/5', requestBody)
+            .expectBody(expectedResponseBody)
+            .expectStatus(400)
+            .run(done); 
+    })
+
+    it('should raise appropriate error if body is missing', function(done) {
         var expectedResponseBody = {
             message: "The request body was not valid.",
             details: {
@@ -26,7 +49,7 @@ describe('when you send put request to simple resource which validates the body'
             }
         };
 
-        request(app).put('/puppies/5')
+        request(app).put('/puppies/5', null)
             .expectBody(expectedResponseBody)
             .expectStatus(400)
             .run(done); 
