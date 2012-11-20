29c04b0ba36cb425f97e31611fb574484ee9d33b
Colin Jack <colin.jack@gmail.com>
2012-11-20 14:07:18 +0000

    Filling in some more tests.

--- spec/unit/resource/Resource/methodDefinitions/errors_spec.js
+++ spec/unit/resource/Resource/methodDefinitions/errors_spec.js
@@ -1,22 +1,64 @@
+var Resource = resourcedLib.require('Resource');
+
 describe('errors specifying resource method', function() {
-    // Consider flatiron / revalidator',
     describe('when the resource method definition has no verb specified and conventions do not cover it', function() {
-        it("should throw an exception")
+        it("should throw an exception", function() {
+            var invalidHandlerDefinition = {
+                bob: function() { }
+            };
+
+            var resourceDefinition = createValidResourceDefinition();
+            resourceDefinition.respondsTo.push(invalidHandlerDefinition);
+
+            assert.throws(createResourceFunction(resourceDefinition), /Could not work out what http verb/);
+        });
     });
     describe('when the collection resource method definition has no verb specified and conventions do not cover it', function() {
         it("should throw an exception")
     });
 
     describe('when the resource method definition has more than one function', function() {
-        it("should throw an exception")
+        it("should throw an exception", function() {
+            var invalidHandlerDefinition = {
+                get: function() { },
+                put: function() { }
+            };
+
+            var resourceDefinition = createValidResourceDefinition();
+            resourceDefinition.respondsTo.push(invalidHandlerDefinition);
+
+            assert.throws(createResourceFunction(resourceDefinition), /Each object in the responds to must contain one function./);
+        });
     });
 
-    describe('when the resource method definition has URL specified but its invalid', function() {
-        it("should throw an exception")
+    describe('when the resource method definition has URL specified but its null', function() {
+        it("should throw an exception", function() {
+            var resourceDefinition = createValidResourceDefinition();
+            resourceDefinition.url = null;
+
+            assert.throws(createResourceFunction(resourceDefinition), /Please provide a URL/);
+        });
     });
 
     describe('when the resource method definition has unexpected properties', function() {
         it("should throw an exception")
     });
 
+    function createValidResourceDefinition() {
+        return {
+            url: "/people",
+            respondsTo: [
+                {
+                    get: function() {
+                    }
+                }
+            ]
+        }
+    }
+
+    function createResourceFunction(resourceDefinition) {
+        return function() {
+            return Resource(resourceDefinition);
+        }
+    }
  });
