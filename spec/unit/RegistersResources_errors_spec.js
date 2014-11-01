34faa5c56dccfab79573fbbdb8f6f07245efb0ea
Colin Jack <colin.jack@gmail.com>
2014-11-01 14:39:18 +0000

    Unit tests passing.

--- spec/unit/RegistersResources_errors_spec.js
+++ spec/unit/RegistersResources_errors_spec.js
@@ -1,74 +1,96 @@
 ﻿var assert = require('chai').assert;
 var resourced = require('require-namespace').resourced;
 var RegistersResources = resourced.require('RegistersResources');
+var Q = require('Q');
 
 describe('RegistersResources', function () {
+    var thrownError = null;
+
+    var catchError = function catchError(path, express) {
+        return function (done) {
+            thrownError = null;
+            
+            Q.spawn(function * () {
+                try {
+                    yield * RegistersResources.registerAllInDirectory(path, express)
+                    done("Expected error but non occurred", null);
+                }
+                catch (e) {
+                    thrownError = e;
+                    done(null, thrownError);
+                }
+            });
+        }
+    }
+
     describe("invalid express instance provided", function () {
         describe("when given null express instance", function () {
+            beforeEach(catchError("/util", null))
+
             it("should raise an exception", function () {
-                assert.throws(function () {
-                    RegistersResources.registerAllInDirectory("/util", null)
-                }, /You must provide the express instance to configure./)
+                assert.equal(thrownError.message, "You must provide the express instance to configure.")
             })
         });
         
         describe("when given undefined express instance", function () {
+            beforeEach(catchError("/util", undefined))
+            
             it("should raise an exception", function () {
-                assert.throws(function () {
-                    RegistersResources.registerAllInDirectory("/util", null)
-                }, /You must provide the express instance to configure./)
+                assert.equal(thrownError.message, "You must provide the express instance to configure.")
             })
         });
         
         describe("when given express instance with no use method", function () {
+            beforeEach(catchError("/util", {}))
+            
             it("should raise an exception", function () {
-                assert.throws(function () {
-                    RegistersResources.registerAllInDirectory("/util", {})
-                }, /You must provide the express instance to configure./)
+                assert.equal(thrownError.message, "You must provide the express instance to configure.")
             })
         });
     });
     
     describe("invalid directory path provided", function () {
+        var MissingDirectoryPathMessage = "You must provide the directory path to scan.";
+
         var validFakeExpressInstance = { use: function () { } };
         
         describe("when given dummy express instance but null directory", function () {
+            beforeEach(catchError(null, validFakeExpressInstance))
+            
             it("should raise an exception", function () {
-                assert.throws(function () {
-                    RegistersResources.registerAllInDirectory(null, validFakeExpressInstance)
-                }, /You must provide the directory path to scan./)
+                assert.equal(thrownError.message, MissingDirectoryPathMessage)
             })
         });
 
         describe("when given dummy express instance but undefined directory", function () {
+            beforeEach(catchError(undefined, validFakeExpressInstance))
+            
             it("should raise an exception", function () {
-                assert.throws(function () {
-                    RegistersResources.registerAllInDirectory(undefined, validFakeExpressInstance)
-                }, /You must provide the directory path to scan./)
+                assert.equal(thrownError.message, MissingDirectoryPathMessage)
             })
         });
 
         describe("when given dummy express instance but non-string directory", function () {
+            beforeEach(catchError(5, validFakeExpressInstance))
+            
             it("should raise an exception", function () {
-                assert.throws(function () {
-                    RegistersResources.registerAllInDirectory(5, validFakeExpressInstance)
-                }, /You must provide the directory path to scan./)
+                assert.equal(thrownError.message, MissingDirectoryPathMessage)
             })
         });
 
         describe("when given dummy express instance but empty directory", function () {
+            beforeEach(catchError("", validFakeExpressInstance))
+            
             it("should raise an exception", function () {
-                assert.throws(function () {
-                    RegistersResources.registerAllInDirectory("", validFakeExpressInstance)
-                }, /You must provide the directory path to scan./)
+                assert.equal(thrownError.message, MissingDirectoryPathMessage)
             })
         });
 
         describe("when given dummy express instance but directory path is to non-existent directory", function () {
+            beforeEach(catchError("c:\\foo\\bar\\this\\is\\not\\going\\to\\exist", validFakeExpressInstance))
+            
             it("should raise an exception", function () {
-                assert.throws(function () {
-                    RegistersResources.registerAllInDirectory("c:\\foo\\bar\\this\\is\\not\\going\\to\\exist", validFakeExpressInstance)
-                }, "The specified directory does not exist c:\\foo\\bar\\this\\is\\not\\going\\to\\exist")
+                assert.equal(thrownError.message, "The specified directory does not exist c:\\foo\\bar\\this\\is\\not\\going\\to\\exist.")
             })
         });
     });
