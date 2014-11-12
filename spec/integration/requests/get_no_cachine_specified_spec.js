e9ea925d0c11743d57ed47cb1aeb0e322be5221c
Colin Jack <colin.jack@gmail.com>
2014-11-12 21:46:29 +0000

    Moved invoke_mocha, it seemed to be interfering with tests.

--- spec/integration/requests/get_no_cachine_specified_spec.js
+++ spec/integration/requests/get_no_cachine_specified_spec.js
@@ -1,42 +1,42 @@
-﻿//var resourceTest = require('testresources')
-//var fixture = require('./../integrationTestFixture')
+﻿var resourceTest = require('testresources')
+var fixture = require('./../integrationTestFixture')
 
-//var assert = fixture.assert;
-//var superAgent = require('superagent');
+var assert = fixture.assert;
+var superAgent = require('superagent');
 
-//describe('when you test a get request and resource returns json which is not cacheable', function () {
-//    var request;
-//    var expectedBody;
-//    var expectedAddressBody;
+describe('when you test a get request and resource returns json which is not cacheable', function () {
+   var request;
+   var expectedBody;
+   var expectedAddressBody;
     
-//    before(fixture.ensureSetup);
+   before(fixture.ensureSetup);
     
-//    beforeEach(function () {
-//        request = superAgent.get(fixture.server.fullUrl('/people/5'));
+   beforeEach(function () {
+       request = superAgent.get(fixture.server.fullUrl('/people/5'));
         
-//        expectedBody = {
-//            firstName : "Colin",
-//            secondName : "Jack",
-//            id : '5',
-//            address: "http://127.0.0.1:" + fixture.port + "/address/5"
-//        }
+       expectedBody = {
+           firstName : "Colin",
+           secondName : "Jack",
+           id : '5',
+           address: "http://127.0.0.1:" + fixture.port + "/address/5"
+       }
         
-//        expectedAddressBody = {
-//            "House Number": 72,
-//            "Stree Name": "Fox Lane",
-//            "Town": "Edinburgh",
-//            "PostCode": "EH99 7JJ"
-//        };
-//    });
+       expectedAddressBody = {
+           "House Number": 72,
+           "Stree Name": "Fox Lane",
+           "Town": "Edinburgh",
+           "PostCode": "EH99 7JJ"
+       };
+   });
     
-//    it('should respond with appropriate body', function () {
-//        return resourceTest(request)
-//                    .expectBody(expectedBody)
-//                    .expectNotCached()
-//                    .followLink("address")
-//                         .expectBody(expectedAddressBody)
-//                         .expectCachedForever("public")
-//                         .endLink()
-//                    .run(fixture.server);
-//    }); 
-//});
+   it('should respond with appropriate body', function () {
+       return resourceTest(request)
+                   .expectBody(expectedBody)
+                   .expectNotCached()
+                   .followLink("address")
+                        .expectBody(expectedAddressBody)
+                        .expectCachedForever("public")
+                        .endLink()
+                   .run(fixture.server);
+   }); 
+});
